fa4264336cfe9c427890cbc9206edf9d013139eb
Andrea Jao <ndrjao@gmail.com>
2024-05-01 12:33:01 -0700

    Created a User class

--- blogly/models.py
+++ blogly/models.py
@@ -1,1 +1,36 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+dbx = db.session.execute
+
+
+class User(db.Model):
+    """creating a User class"""
+
+    __tablename__ = "users"
+
+    id = db.mapped_column(
+        db.Integer,
+        db.Identity(),
+        primary_key=True,
+    )
+
+    first_name = db.mapped_column(
+        db.string(50),
+        nullable=False
+    )
+
+    last_name = db.mapped_column(
+        db.string(50),
+        nullable=False
+    )
+
+    image_url = db.mapped_column(
+        db.string(200),
+        unique=True
+    )
+
+    def get_full_name(self):
+        return self.first_name + self.last_name
