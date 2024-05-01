e0d01018126363d37e486e723f51d800a743e919
Andrea Jao <ndrjao@gmail.com>
2024-05-01 15:26:02 -0700

    updated app.py

--- blogly/app.py
+++ blogly/app.py
@@ -2,8 +2,15 @@
 
 import os
 
-from flask import Flask
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from werkzeug.exceptions import NotFound
+
+from models import db, dbx, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", 'postgresql:///blogly')
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_RECORD_QUERIES'] = True
+db.init_app(app)
